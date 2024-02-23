cab3425993778d0899f42827aba7988b274a2193
Alistair Seymore <25964915+gen-ass@users.noreply.github.com>
2024-02-23 19:30:55 +0100

    adding odd excercise

---
+++ Is-it-not-odd.js
@@ -0,0 +1,2 @@
+// Write a function that takes one integer argument, which may be positive, negative, or zero. This method returns true if the number's absolute value is odd. You may assume that the argument is a valid integer value.
+
