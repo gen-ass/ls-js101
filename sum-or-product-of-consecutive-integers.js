1558bcf6f9a0d49d1d32ddb213f3e90d14a198da
Alistair Seymore <25964915+gen-ass@users.noreply.github.com>
2024-03-05 13:07:00 +0100

    excercise: Explored the Array.prototype.reduce() method

--- sum-or-product-of-consecutive-integers.js
+++ sum-or-product-of-consecutive-integers.js
@@ -34,23 +34,66 @@ Algorithm
 Code
 */
 
+// Nr 1
 let rlSync = require('readline-sync');
 
-let integeRange = parseInt(rlSync.question('Please enter an integer greater than 0:\n'));
-let productSum = rlSync.question('Enter "s" to compute the sum, or "p" to compute the product.\n').toLowerCase();
-let sum = 0;
-let product = 1;
+// let integeRange = parseInt(rlSync.question('Please enter an integer greater than 0:\n'));
+// let productSum = rlSync.question('Enter "s" to compute the sum, or "p" to compute the product.\n').toLowerCase();
+// let sum = 0;
+// let product = 1;
 
-for (let i = 1; i <= integeRange; i += 1) {
-	if (productSum === "s") {
-		sum += i;
-	} else {
-		product *= i;
-	}
+// for (let i = 1; i <= integeRange; i += 1) {
+// 	if (productSum === "s") {
+// 		sum += i;
+// 	} else {
+// 		product *= i;
+// 	}
+// }
+
+// if (productSum === "s") {
+// 	console.log(`The sum of the integers between 1 and ${integeRange}, ${sum}`);
+// } else {
+// 	console.log(`The product of the integers between 1 and ${integeRange}, ${product}`);
+// }
+
+// Nr 2
+let input = rlSync.question('Please enter a list of integers (as a single number, separated by spaces, or separated by commas):\n');
+
+let integerRange;
+
+if (!isNaN(input)) {
+    // If the input is a single number, split it into individual digits
+    integerRange = Array.from(input, Number);
+} else if (input.includes(',')) {
+    // If the input contains commas, split by commas
+    integerRange = input.split(',').map(Number);
+} else {
+    // Otherwise, split by spaces
+    integerRange = input.split(' ').map(Number);
+}
+
+if (integerRange.some(isNaN)) {
+    console.log('Invalid input. Please only enter numbers.');
+} else {
+    console.log(integerRange);
+}
+
+let productSum = rlSync.question('Enter "s" to compute the sum, or "p" to compute the product.\n').toLowerCase();
+
+function calculateSum(integerArray) {
+	return integerArray.reduce((sum, current) => sum + current, 0);
+}
+
+function calculateProduct(integerArray) {
+	return integerArray.reduce((product, current) => product * current, 1);
 }
 
 if (productSum === "s") {
-	console.log(`The sum of the integers between 1 and ${integeRange}, ${sum}`);
+	let sum = calculateSum(integerRange);
+	console.log(`The sum of the integers between 1 and ${integerRange} is ${sum}`);
 } else {
-	console.log(`The product of the integers between 1 and ${integeRange}, ${product}`);
+	let product = calculateProduct(integerRange);
+	console.log(`The product of the integers between 1 and ${integerRange} is ${product}`);
 }
+
+// 
